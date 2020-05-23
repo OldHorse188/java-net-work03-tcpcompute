ebc2ff529361f824366ff59518681bad086e8191
OldHorse188 <MaJiaHao188@foxmail.com>
2020-05-24 00:02:22 +0800

    添加注释

--- src/ComputeClient.java
+++ src/ComputeClient.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.net.*;
 
-
+// 客户端
 public class ComputeClient extends Application {
 
     TextArea t1;
@@ -26,46 +26,57 @@ public class ComputeClient extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+        // 第一个文本域（用于结果显示）
         t1 = new TextArea();
         t1.setEditable(false);
 
+        // 第二个文本域（用于输入数据）
         t2 = new TextArea();
         t2.setPrefRowCount(3);
         t2.setPromptText("1.按行区分多个输入 2.空格、空行将被忽略");
 
+        // 提交按钮
         Button bt = new Button("计算");
 
+        // 使用 HBox 装按钮 并将按钮靠右对齐
         HBox hb = new HBox();
         hb.setAlignment(Pos.BASELINE_RIGHT);
         hb.getChildren().add(bt);
 
+        // 最底部 装 所有部件 的 BorderPane 布局
         BorderPane root = new BorderPane();
-
         root.setTop(t1);
         root.setCenter(t2);
         root.setBottom(hb);
 
+        // 设置Stage框
         primaryStage.setTitle("客户机");
         primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
 
+        // 启动 Socket 线程（另开线程的目的是防止图形界面卡死）
         new ComputeThread().start();
 
+        // 提交按钮 的 鼠标点击事件
         bt.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 try {
+                    // 将 输入内容 按行分割
                     String[] strs = t2.getText().split("\n");
                     for (String str:strs) {
+                        // 将 输入内容 提交给 Server
                         out.writeUTF(str);
 
+                        // 在第一个文本域显示 非空的输入内容 及 对应的结果
                         String result = in.readUTF();
                         if (!result.equals("")) {
                             t1.appendText("输入："+str+"\n");
                             t1.appendText("结果："+result+"\n\n");
                         }
                     }
+                    // 提交内容 并 获取结果后，清除输入框
                     t2.clear();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -86,6 +97,7 @@ public class ComputeClient extends Application {
         public void run() {
 
             try {
+                // 与服务端建立连接 并获取输入输出流
                 socket = new Socket("127.0.0.1", 2020);
                 out = new DataOutputStream(socket.getOutputStream());
                 in = new DataInputStream(socket.getInputStream());
--- src/ComputeServer.java
+++ src/ComputeServer.java
@@ -3,11 +3,12 @@ import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
+// 服务端
 public class ComputeServer {
     public static void main(String[] args) {
 
         try {
+            // 监听2020端口，并使用 newFixedThreadPool 线程池限制同时运行的线程
             ServerSocket ss = new ServerSocket(2020);
             ExecutorService fixedThreadPool = Executors.newFixedThreadPool(1);
             while (true) {
@@ -37,13 +38,17 @@ class SocketThread extends Thread {
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
             while (true) {
+                // flag 用于标记字符串的合法性
                 boolean flag = true;
+                // str 存储一条来自客户端提交的内容
                 String str = in.readUTF();
 
+                // 对 空字符串 返回 空结果
                 if (!str.equals("")) {
                     // 判断字符合法性
                     char[] chars = str.toCharArray();
                     for (char chr:chars) {
+                        // 限制字符种类
                         if (!((chr >= '0' && chr <= '9') || chr == '\n' || chr == ',')) {
                             flag = false;
                             out.writeUTF("数据格式有误");
@@ -51,13 +56,18 @@ class SocketThread extends Thread {
                         }
                     }
                     if (flag) {
+                        // 去除 空格和多余的回车，并依据逗号分割获取数值
                         str = str.replace(" ", "");
                         str = str.replace("\n", "");
                         String[] num = str.split(",");
 
+                        // 和
                         double sum = 0;
+                        // 平均数
                         double ave = 0;
+                        // 方差
                         double var = 0;
+
                         for (String sr:num) {
                             sum += Double.parseDouble(sr);
                         }
